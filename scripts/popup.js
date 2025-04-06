08e96a9ccb9fa49103c007a18116c0ea7b1deedc
Samik Pradhan <samikpradhan@gmail.com>
2025-04-05 19:36:47 -0700

    created Video class to store data about the video

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,16 @@
+class Video {
+    constructor(title, category = "NaN") {
+        this.title = title;
+        this.category = category;
+        this.start_time = Date.now();
+        this.end_time = null;
+    }
+
+    toString() {
+        return `<Video: title="${this.title}", category="${this.category}", start_time="${this.start_time}", end_time=${this.end_time}>`
+    }
+}
+
 chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     chrome.scripting.executeScript(
         {
@@ -8,7 +21,7 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             if (chrome.runtime.lastError) {
                 document.getElementById('title').textContent = "Error: " + chrome.runtime.lastError.message;
             } else if (results && results[0] && results[0].result) {
-                document.getElementById('title').textContent = results[0].result;
+                document.getElementById('title').textContent = new Video(results[0].result).toString();
             } else {
                 document.getElementById('title').textContent = "Title not found.";
             }
