ab05cb331611ce52995325c1247d38b6c4b8089a
Samik Pradhan <samikpradhan@gmail.com>
2025-04-05 19:50:49 -0700

    created Node class

--- scripts/popup.js
+++ scripts/popup.js
@@ -7,7 +7,19 @@ class Video {
     }
 
     toString() {
-        return `<Video: title="${this.title}", category="${this.category}", start_time="${this.start_time}", end_time=${this.end_time}>`
+        return `<Video: title="${this.title}", category="${this.category}", start_time="${this.start_time}", end_time=${this.end_time}>`;
+    }
+}
+
+class Node {
+    constructor(video = null, next = null, prev = null) {
+        this.video = video;
+        this.next = next;
+        this.prev = prev;
+    }
+
+    toString() {
+        return `<Node: Video=${this.video}, next=${this.next}, prev=${this.prev}>`;
     }
 }
 
@@ -21,7 +33,10 @@ chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
             if (chrome.runtime.lastError) {
                 document.getElementById('title').textContent = "Error: " + chrome.runtime.lastError.message;
             } else if (results && results[0] && results[0].result) {
-                document.getElementById('title').textContent = new Video(results[0].result).toString();
+                const r = results[0].results;
+                var v = new Video(r);
+                var n = new Node(v);
+                document.getElementById('title').textContent = n;
             } else {
                 document.getElementById('title').textContent = "Title not found.";
             }
